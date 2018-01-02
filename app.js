e8863495cfed397d1f514d71c5d9404dca7d5560
zcoding <wu.zijie.software@gmail.com>
2018-01-02 21:07:45 +0800

    update examples

---
+++ app.js
@@ -0,0 +1,49 @@
+ChimeePlayer.install(ChimeeSnapshot)
+
+const snapshotContainer = document.querySelector('.snapshot-container')
+
+var player = new ChimeePlayer({
+  wrapper: '.chimee-container',
+  src: './examples/simple/1.mp4',
+  isLive: false,
+  box:'native',
+  autoplay: false,
+  controls: true,
+  autoplay: true,
+  width: 640,
+  height: 360,
+  plugin: [{
+    name: ChimeeSnapshot.name,
+    // watermark: {
+    //   text: 'chimee-plugin-snapshot',
+    //   x: -300,
+    //   y: -100,
+    //   rotate: 15,
+    //   fontColor: 'rgba(255, 255, 255, 0.5)',
+    //   fontSize: 22
+    // },
+    watermark: {
+      image: './examples/simple/chimee-logo.png',
+      x: -140,
+      y: -70,
+      opacity: 0.8,
+      rotate: 10
+    },
+    snapshotted(snapshot) {
+      const image = new Image()
+      image.src = snapshot.src
+      snapshotContainer.appendChild(image)
+      console.log(snapshot)
+    }
+  }]
+})
+
+let count = 0
+const timer = setInterval(() => {
+  if (count >= 8) {
+    clearTimeout(timer)
+    return false
+  }
+  player.$plugins.snapshot.takeSnapshot()
+  count++
+}, 1000)
